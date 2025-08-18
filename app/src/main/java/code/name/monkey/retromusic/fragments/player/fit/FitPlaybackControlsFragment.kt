48e844bacb4bea5da8209038671e1aa2c6271198
effinmr <effinmr@gmail.com>
2025-08-18 23:57:18 +0530

    Update FitPlaybackControlsFragment.kt

--- app/src/main/java/code/name/monkey/retromusic/fragments/player/fit/FitPlaybackControlsFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/player/fit/FitPlaybackControlsFragment.kt
@@ -84,34 +84,16 @@ class FitPlaybackControlsFragment :
         binding.title.text = song.title
         
         val artistName = song.artistName?.trim()
-        val delimiters = PreferenceUtil.artistDelimiters
-        
-        val allArtists: List<String> = (song.allArtists?.split(",") ?: emptyList<String>())
+        val allArtists = listOfNotNull(song.albumArtist, song.artistName)
             .map { it.trim() }
             .filter { it.isNotEmpty() }
+            .distinct()
             
-        individualArtists = if (delimiters.isBlank()) {
-            allArtists
-        } else {
-            val splitNames = allArtists
-                .flatMap { artist ->
-                    artist.split(*(
-                            delimiters.split(",")
-                            .map { it.trim() }
-                            .map { if (it.isEmpty()) "," else it }
-                            .distinct()
-                            .toTypedArray()
-                    )).map { it.trim() }
-                }
-                .filter { it.isNotEmpty() }
-                .distinct()
-            (allArtists + splitNames)
-                .filter { it.isNotEmpty() }
-                .distinct()
-        }
+        individualArtists = allArtists
         
         // Always display the full artist name string
-        binding.text.text = song.allArtists
+        binding.text.text = allArtists
+            .joinToString(", ")
         
         if (PreferenceUtil.isSongInfo) {
             binding.songInfo.text = getSongInfo(song)
